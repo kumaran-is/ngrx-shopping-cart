419067dbce6a65cf511d76b77922074b22e1a371
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-02-14 22:45:00 -0500

    updated layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { MatButtonModule, MatFormFieldModule, MatIconModule, MatInputModule, MatListModule, MatSidenavModule, MatToolbarModule } from '@angular/material';
-import { HeaderComponent } from './layout/header/header.component';
 import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
 import { MainComponent } from './layout/main/main.component';
 @NgModule({
   declarations: [
@@ -34,7 +34,9 @@ import { MainComponent } from './layout/main/main.component';
     MatIconModule,
     MatInputModule,
     MatListModule,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ]
 })
 export class CoreModule { }
