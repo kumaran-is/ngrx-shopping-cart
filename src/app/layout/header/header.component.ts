f3317eb22a747c0f4f845ffdc6e9aaa871d47b5b
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-02-19 14:08:06 -0500

    fixed menu route

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -8,14 +7,6 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent {
 
-constructor(private router: Router) {}
-
-public goCart() {
-  this.router.navigate(['cart']);
-}
-
-public goHome() {
-  this.router.navigate(['home']);
-}
+constructor() {}
 
 }
