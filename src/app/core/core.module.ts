ce2b62cd176431ba7d977f684dad6c9e90afba50
Kumaran Iyyasamy Srinivasan <Kumaran.X.Iyyasamy.Srinivasan.-ND@disney.com>
2019-02-20 13:36:01 -0500

    fixed sticky footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
 
 @NgModule({
   imports: [
@@ -8,4 +8,13 @@ import { NgModule } from '@angular/core';
   exports: [
   ]
 })
-export class CoreModule { }
+export class CoreModule {
+  /* make sure CoreModule is imported only by one NgModule i.e the AppModule
+  and makes all the services(providers) under CoreMode as application-wide
+  singleton service */
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    if (parentModule) {
+      throw new Error('CoreModule is already loaded. Import only in AppModule');
+    }
+  }
+}
